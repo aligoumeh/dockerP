0c8a251ea84158d5098f2aef8bf2df3fe23c130f
Ali Goumeh <475441@student.saxion.nl>
2021-04-07 02:36:21 +0200

    docker compose to run everything

--- backend/src/model/person_controller.js
+++ backend/src/model/person_controller.js
@@ -15,14 +15,14 @@ class PersonController {
     })
 
     let person2 = new PersonModel({
-      name: 'Fleur',
+      name: 'aaaa',
       street: 'van Galenstraat',
       city: 'Enschede',
       country: 'Netherlands'
     })
 
     let person3 = new PersonModel({
-      name: 'Jelle',
+      name: 'bbbbb',
       street: 'Handelskade',
       city: 'Deventer',
       country: 'Netherlands'
@@ -35,10 +35,43 @@ class PersonController {
       country: 'Netherlands'
     })
 
+    let person5 = new PersonModel({
+      name: 'Ali',
+      street: 'van Galenstraat',
+      city: 'Enschede',
+      country: 'Netherlands'
+    })
+
+    let person6 = new PersonModel({
+      name: 'Lucia',
+      street: 'van Galenstraat',
+      city: 'Enschede',
+      country: 'Netherlands'
+    })
+    
+    let person7 = new PersonModel({
+      name: 'Alice',
+      street: 'Wonderland',
+      city: 'unknown',
+      country: 'far away'
+    })
+
+    let person8 = new PersonModel({
+      name: 'fictional name',
+      street: 'kersengaarde',
+      city: 'paris',
+      country: 'France'
+    })
+    
+
     this._save(person1);
     this._save(person2);
     this._save(person3);
     this._save(person4);
+    this._save(person5);
+    this._save(person6);
+    this._save(person7);
+    this._save(person8);
   }
 
   _save(person) {
